26c482a5966e501e714218a4a0dac603515b7100
Donna <donnajun1214@gmail.com>
2022-05-24 17:19:01 +0900

    structure, custom data type in swift

---
+++ Quiz App/Question.swift
@@ -0,0 +1,17 @@
+//
+//  Question.swift
+//  Quiz App
+//
+//  Created by Donna on 2022/05/24.
+//
+
+import Foundation
+
+struct Question{
+    let text:String
+    let answer:String
+    init(q:String,a:String){
+        text = q
+        answer = a
+    }
+}
